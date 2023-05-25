a2e50478d2a66af00a8fe59e630cee9f4f19ff6c
woodosa <dndydgk12@gmail.com>
2023-05-25 18:15:18 +0900

    05.25

--- 36.js-문서객체/study10.js
+++ 36.js-문서객체/study10.js
@@ -1,14 +1,56 @@
 window.onload= function(){
-
+  
     let slider1 = new Swiper('.swiper',{
         loop: true,
+        // slidesPerView:3,//슬라이드 개수 최대 반만큼만 적용가능
+        spaceBetween:40,
         pagination: {
             el: ".swiper-pagination",
-            type: "progressbar",
+            type: "bullets",
+            clickable:true,
+            dynamicBullets: true,
           },
           navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".BtnNext",
+            prevEl: ".BtnPrev",
           },
+          autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+            // pauseOnMouseEnter:true
+            
+          },
+          speed:2000,
+          breakpoints:{
+            320:{
+              slidesPerView:1,
+              spaceBetween:0
+            },
+            768:{
+              slidesPerView:2,
+              spaceBetween:10
+            },
+            1024:{
+              slidesPerView:3,
+              spaceBetween:30
+            }
+          }
+          // on: {
+          //   autoplayTimeLeft(s, time, progress) {
+          //     progressCircle.style.setProperty("--progress", 1 - progress);
+          //     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+          //   }
+          // }
+          // navigation: {
+          //   nextEl: ".swiper-button-next",
+          //   prevEl: ".swiper-button-prev",
+          // },
+          
         });
+        document.querySelector(".station").addEventListener("mouseover",function(){
+          slider1.autoplay.stop()
+        })
+        document.querySelector(".station").addEventListener("mouseout",function(){
+          slider1.autoplay.start()
+        })
 }
