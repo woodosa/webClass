c5e19f8fb8cffec554a93a5aca69ec7cee40e643
woodosa <dndydgk12@gmail.com>
2023-06-09 18:49:34 +0900

    06.09

--- redesign/JS/Menu.js
+++ redesign/JS/Menu.js
@@ -6,41 +6,18 @@ $(document).ready(function(){
             $(".blackcover").addClass("on")
             $(".menubutton").addClass("on")
             $(".headL>i").html("닫기")
-            $(".menubutton").children().removeClass("on")
+            $(".menubutton").find("li").removeClass("on")
             
         }else{
-            $(".menubutton").children().removeClass("on")
+            $(".menubutton").find("li").removeClass("on")
             $(this).removeClass("on")
             $(".blackcover").removeClass("on")
             $(".menubutton").removeClass("on")
             $(".headL>i").html("메뉴")
         }
     })
-    $(".menubottom>ul>li").click(function(){
-        // let menuDepth1 = $(this).children("ul").children().length
-        // let menuDepth2 = $(this).children("div").children("ul").children().length
-        // if(menuDepth1!==0){
-        //     if($(this.hasClass("on")==true)){
-        //         $(this).css("height","80px")
-        //         $(this).removeClass("on")
-        //     }else{
-        //         $(".menubottom>ul>li").css("height","8  0px")
-        //         $(".menubottom>ul>li").removeClass("on")
-        //         $(this).css("height",(menuDepth1+1)*80+"px")
-        //         $(this).addClass("on")
-        //     }
-        // }
-        // if(menuDepth2!==0){if($(this.hasClass("on")==true)){
-        //     $(this).css("height","80px")
-        //     $(this).removeClass("on")
-        // }else{
-        //     $(".menubottom>ul>li").css("height","80px")
-        //     $(".menubottom>ul>li").removeClass("on")
-        //     $(this).css("height",(menuDepth2+1)*80+"px")
-        //     $(this).addClass("on")
-        // }}
-        // $(".menubottom>ul>li").children().removeClass("on")
-        
+
+    $(".menubottom>ul>li").click(function(){      
         if($(this).hasClass("on")==true){
             $(this).removeClass("on")
         }else{
@@ -49,31 +26,31 @@ $(document).ready(function(){
             
         }
         
-        // !$(this).removeClass("on")
         
         
     })
-    $(".menubottom>ul>li>ul>li").click(function(){
+    $(".menubottom>ul>li>ul").click(function(){
         return false
 
     })
-    $(".menubottom>ul>li>ul>li").click(function(){
-        if($(this).hasClass("on")==true){
-        $(this).removeClass("on")
-    }else{
-        $(".menubottom>ul>li>ul>li").removeClass("on")
-        $(this).addClass("on")
-        $(this).css("color","#ddd")
+
+    menuClick(".menubottom>ul>li>ul>li")
+
+    
+    $(".menubottom>ul>li>ul>li>div>ul").click(function(){
+        return false
+
+    })
+
+    function menuClick(menuDepth){
+        $(menuDepth).click(function(){
+            if($(this).hasClass("on")==true){
+            $(this).removeClass("on")
+        }else{
+            $(menuDepth).removeClass("on")
+            $(this).addClass("on")
+            $(this).css("color","#ddd")
+        }
+        })  
     }
-    })  
-    $(".menubottom>ul>li>ul>li>div>ul>li").click(function(){
-        return false
-
-    })
-
-
-    // $(".blackcover").click(function(){
-    //     return false;
-    // })
-
 })
---
+++ redesign/JS/defaultSlider.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    let slider1 = new Swiper(".main_otherbrand_brand", {
+
+        loop:true,
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable:true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay:{
+        delay: 3500,
+          disableOnInteraction: false,
+          // pauseOnMouseEnter:true
+
+        },
+        speed:2000,
+    });
+
+
+})
