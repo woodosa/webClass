e62df54c065a9c411fdade3e5041cb5f0eee1eaa
woodosa <dndydgk12@gmail.com>
2023-06-14 18:42:12 +0900

    06.14

--- redesign/JS/banner_swiper.js
+++ redesign/JS/banner_swiper.js
@@ -16,7 +16,7 @@ window.onload=function(){
     let BannerSlider = new Swiper(".OB_mainBanner", {
       loop:true,
       pagination: {
-        el: ".swiper-pagination",
+        el: ".pag1",
         dynamicBullets: true,
         clickable:true
       },
@@ -35,6 +35,26 @@ window.onload=function(){
       
 
     });
+    let slider1 = new Swiper(".main_otherbrand_brand", {
+
+      loop:true,
+    pagination: {
+      el: ".pag2",
+      dynamicBullets: true,
+      clickable:true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay:{
+      delay: 3500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter:true
+
+      },
+      speed:3500,
+  });
     
     // let Bannerslider = new Swiper(".OB_mainBanner",{
     //   loop: true,
--- redesign/JS/mainpage.js
+++ redesign/JS/mainpage.js
@@ -1,37 +1,89 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         let winst = $(window).scrollTop() // 스크롤바 위에서 얼만큼 내려왔는지 계산
-        let winHeight = $(window).height()*0.65 
-        let winScrollSet = winst+winHeight
-        console.log(winScrollSet)
+        let winHeight = $(window).height() * 0.65
+        let winScrollSet = winst + winHeight
+        // console.log(winScrollSet)
 
-        $(".BG,.videoAnimate,.mltr,.mrtl,.mttb,.mbtt").each(function(){
-            if(winScrollSet>$(this).offset().top){
+        $(".BG,.videoAnimate,.mltr,.mrtl,.mttb,.mbtt").each(function () {
+            if (winScrollSet > $(this).offset().top) {
                 $(this).addClass("on")
-            }else{
+            } else {
                 $(this).removeClass("on")
             }
         })
-        
-        if(winScrollSet>1500){
+        if (winScrollSet > 1500) {
             $("header").addClass("on")
-        }else{
+        } else {
             $("header").removeClass("on")
         }
 
 
-        if(winScrollSet>0){
+        if (winScrollSet > 0) {
             $(".main_controler>ul>li").removeClass("on")
             $(".main_controler>ul>li").eq(0).addClass("on")
         }
-        if(winScrollSet>3129.85){
+        if (winScrollSet > 3129.85) {
             $(".main_controler>ul>li").removeClass("on")
             $(".main_controler>ul>li").eq(1).addClass("on")
 
-        }if(winScrollSet>5129.85){
+        } if (winScrollSet > 5129.85) {
             $(".main_controler>ul>li").removeClass("on")
             $(".main_controler>ul>li").eq(2).addClass("on")
 
         }
     })
+
+
+    let videoTop = $(".aniVideo").offset().top
+    
+    $(window).scroll(function () {
+        // aniVideo
+        let winst = $(window).scrollTop() // 스크롤바 위에서 얼만큼 내려왔는지 계산
+        let winHeight = $(window).height()
+        
+        let targetHeight = $(".aniVideo").height()
+        let transformRatio =(winst-videoTop)/(targetHeight);
+
+        if (winst >= videoTop && winst< videoTop+(targetHeight)/2) {
+            console.log(transformRatio)
+            $(".aniVideo").css("transform","scale("+(1-transformRatio)
+            +")translateY("+(transformRatio*100)+"%)").css("left",8*transformRatio+"vw")
+            
+            $(".aniScrollBox").css("padding-bottom",transformRatio*50+"vh")
+            
+        }
+
+
+    })
+
+
+    // $(".heritage_wrap>div").on("wheel DOMMouseScroll",function(event){
+    // //    console.log(event)
+    //    let E = event.originalEvent
+    //    let delta = 0;
+    //    if(E.detail){
+    //     delta = E.detail * -40
+    //    }else{
+    //     delta = E.wheelDelta
+    //    }
+       
+    //    if(delta<0){
+    //     //마우스 휠을 내렸을 때
+    //     if($(this).next().length){
+    //         let posTop = $(this).next().offset().top
+    //         $("html,body").stop().animate({scrollTop:posTop},1000)
+    //     }
+    //    }else{
+    //     //마우스 휠을 올렸을 때
+    //     if($(this).prev().length!=0){
+    //         let posTop = $(this).prev().offset().top
+    //         $("html,body").stop().animate({scrollTop:posTop},1000)
+    //     }
+    //    }
+    // //    return false
+
+    // })
+
+
 })
