3968befe4645624f8115635db45663fb0e8c25f6
woodosa <dndydgk12@gmail.com>
2023-06-15 18:37:24 +0900

    06.151.51515

--- redesign/JS/bespoke.js
+++ redesign/JS/bespoke.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         let winst = $(window).scrollTop() // 스크롤바 위에서 얼만큼 내려왔는지 계산
         let winHeight = $(window).height()*0.65 
         let winScrollSet = winst+winHeight
-        console.log(winScrollSet)
+        // console.log(winScrollSet)
 
         $(".BG,.videoAnimate,.mltr,.mrtl,.mttb,.mbtt").each(function(){
             if(winScrollSet>$(this).offset().top){
@@ -18,5 +18,25 @@ $(document).ready(function(){
         }else{
             $("header").removeClass("on")
         }
+        // if(winScrollSet>1650){
+        //     $(".bespoke").addClass("on")
+        // }else{
+        //     $(".bespoke").removeClass("on")
+        // }
+
+        // let scrollLeftMoveRatio=$(".mltr").offset().top
     })
+
+    
+    $(window).scroll(function(){
+        let mvTop = $(".mvTitle").offset().top
+        let winst = $(window).scrollTop()
+        let winHeight = $(window).height()*0.65 
+        if(winst+winHeight>=mvTop){
+            console.log(winst+winHeight-mvTop)
+            $(".mvTitle").css("transform","translateX("+(winst+winHeight-mvTop)+"px)")
+        }
+        
+    })
+   
 })
