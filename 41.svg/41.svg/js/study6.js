d44f13c066d7c25519912378ccbe3cb673458f56
woodosa <dndydgk12@gmail.com>
2023-06-30 19:47:26 +0900

    06303030

--- 41.svg/41.svg/js/study6.js
+++ 41.svg/41.svg/js/study6.js
@@ -12,7 +12,10 @@ $(document).ready(function(){
         let movingArea=$(".movingArea")
         let scrollLength=$(".fixedFrame").height()-movingArea.height()
         let percent = (scrollDistance/(scrollLength)*100)
-        console.log(percent)
+        let percent2 = (scrollDistance/(scrollLength)*87.5)
+        let count= Math.floor(percent2/8)// 정수만
+        let count2 =(scrollDistance/(scrollLength)*8)-count// 소수만
+        let idx=Math.round(count)
         if(scrollDistance<0){
             movingArea.css("position","relative").css("top","0px")
         }
@@ -20,6 +23,11 @@ $(document).ready(function(){
             movingArea.css("position","fixed").css("top","0px")
             $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
             $(".per").text(Math.round(percent)+"%")
+            // if(percent>87.5){percent=87.5}
+            $(".train").css("transform","translateX(-"+percent2+"%)")
+            $(".train>li").removeClass("on")
+            $(".train>li").eq(idx).addClass("on")
+            $(".train li b").css("transform","rotateY("+(count*2)*180+"deg)")
         }
         if(scrollDistance>scrollLength){
             movingArea.css("position","relative").css("top",scrollLength+"px")
